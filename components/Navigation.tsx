8e78f2a79ed78167205dddc49d84038c94831ada
Darkened-Z <7eeshan.ahmad@gmail.com>
2026-08-01 18:55:08 +0500

    Nav: increase logo height to h-10/h-12

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -48,7 +48,7 @@ export default function Navigation() {
               width={160}
               height={80}
               priority
-              className="h-8 md:h-9 w-auto"
+              className="h-10 md:h-12 w-auto"
             />
           </span>
         </Link>
